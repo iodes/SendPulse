218d4119fc24b1d0013c738b8ff3e0199c2db021
IODES <iodesme@gmail.com>
2019-04-12 11:39:23 +0900

    템플릿 발송 기능 대응

---
+++ SendPulse.SDK/Models/TemplateEmailData.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SendPulse.SDK.Models
+{
+    public class TemplateEmailData : EmailBase
+    {
+        [JsonIgnore]
+        public string TemplateID { get; set; }
+
+        [JsonIgnore]
+        public Dictionary<string, string> TemplateVariables { get; set; }
+
+        [JsonProperty("template")]
+        internal object TemplateData => new
+        {
+            id = TemplateID,
+            variables = TemplateVariables
+        };
+    }
+}
